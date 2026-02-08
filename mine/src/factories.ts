d27393c7ed3d0cd511c959ac002f95a414c13d22
sequelinquiryDev <sequelinquiry@gmail.com>
2026-02-08 18:25:32 +0200

    Aktualisieren von factories.ts

--- mine/src/factories.ts
+++ mine/src/factories.ts
@@ -1,7 +1,7 @@
 import {
   FeeAmountEnabled as FeeAmountEnabledEvent,
   OwnerChanged as OwnerChangedEvent,
-  PoolCreated as PoolCreatedEvent
+  PoolCreated as V3PoolCreatedEvent
 } from "../generated/UniswapV3Factory/UniswapV3Factory"
 
 import {
@@ -12,16 +12,14 @@ import {
   FeeAmountEnabled,
   OwnerChanged,
   PoolCreated,
-  Pair
+  PairCreated
 } from "../generated/schema"
 
 /******************************
  * Uniswap V3 Factory Handlers *
  ******************************/
 export function handleFeeAmountEnabled(event: FeeAmountEnabledEvent): void {
-  let entity = new FeeAmountEnabled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let entity = new FeeAmountEnabled(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.fee = event.params.fee
   entity.tickSpacing = event.params.tickSpacing
   entity.blockNumber = event.block.number
@@ -31,9 +29,7 @@ export function handleFeeAmountEnabled(event: FeeAmountEnabledEvent): void {
 }
 
 export function handleOwnerChanged(event: OwnerChangedEvent): void {
-  let entity = new OwnerChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let entity = new OwnerChanged(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.oldOwner = event.params.oldOwner
   entity.newOwner = event.params.newOwner
   entity.blockNumber = event.block.number
@@ -42,10 +38,8 @@ export function handleOwnerChanged(event: OwnerChangedEvent): void {
   entity.save()
 }
 
-export function handleV3PoolCreated(event: PoolCreatedEvent): void {
-  let entity = new PoolCreated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+export function handleV3PoolCreated(event: V3PoolCreatedEvent): void {
+  let entity = new PoolCreated(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.token0 = event.params.token0
   entity.token1 = event.params.token1
   entity.fee = event.params.fee
@@ -61,13 +55,11 @@ export function handleV3PoolCreated(event: PoolCreatedEvent): void {
  * V2 Factory Handlers (Uni/Sushi/QuickSwap)
  ******************************/
 export function handleV2PairCreated(event: V2PairCreatedEvent): void {
-  let entity = new Pair(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
+  let entity = new PairCreated(event.transaction.hash.concatI32(event.logIndex.toI32()))
   entity.token0 = event.params.token0
   entity.token1 = event.params.token1
   entity.pair = event.params.pair
-  entity.param3 = event.params[2] // param3 is the last uint256 in PairCreated
+  entity.param3 = event.params[2] // last uint256
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
