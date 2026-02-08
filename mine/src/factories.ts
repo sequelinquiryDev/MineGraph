a2ebb7eb531818ba20986797eb482766635897fd
sequelinquiryDev <sequelinquiry@gmail.com>
2026-02-08 18:48:14 +0200

    Aktualisieren von factories.ts

--- mine/src/factories.ts
+++ mine/src/factories.ts
@@ -1,3 +1,5 @@
+import { Bytes, BigDecimal, BigInt } from "@graphprotocol/graph-ts"
+
 import {
   FeeAmountEnabled as FeeAmountEnabledEvent,
   OwnerChanged as OwnerChangedEvent,
@@ -12,14 +14,17 @@ import {
   FeeAmountEnabled,
   OwnerChanged,
   PoolCreated,
-  PairCreated
+  PairCreated,
+  Pool
 } from "../generated/schema"
 
 /******************************
- * Uniswap V3 Factory Handlers *
+ * Uniswap V3 Factory Handlers
  ******************************/
 export function handleFeeAmountEnabled(event: FeeAmountEnabledEvent): void {
-  let entity = new FeeAmountEnabled(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new FeeAmountEnabled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
   entity.fee = event.params.fee
   entity.tickSpacing = event.params.tickSpacing
   entity.blockNumber = event.block.number
@@ -29,7 +34,9 @@ export function handleFeeAmountEnabled(event: FeeAmountEnabledEvent): void {
 }
 
 export function handleOwnerChanged(event: OwnerChangedEvent): void {
-  let entity = new OwnerChanged(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  let entity = new OwnerChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
   entity.oldOwner = event.params.oldOwner
   entity.newOwner = event.params.newOwner
   entity.blockNumber = event.block.number
@@ -39,7 +46,10 @@ export function handleOwnerChanged(event: OwnerChangedEvent): void {
 }
 
 export function handleV3PoolCreated(event: V3PoolCreatedEvent): void {
-  let entity = new PoolCreated(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  // Event history
+  let entity = new PoolCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
   entity.token0 = event.params.token0
   entity.token1 = event.params.token1
   entity.fee = event.params.fee
@@ -49,19 +59,54 @@ export function handleV3PoolCreated(event: V3PoolCreatedEvent): void {
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
   entity.save()
+
+  // ***** ACTUAL POOL CREATION *****
+  let pool = new Pool(event.params.pool)
+  pool.token0 = event.params.token0
+  pool.token1 = event.params.token1
+  pool.feeTier = event.params.fee
+  pool.tickSpacing = event.params.tickSpacing
+  pool.dexType = "v3"
+  pool.reserve0 = BigDecimal.zero()
+  pool.reserve1 = BigDecimal.zero()
+  pool.reserveUSD = BigDecimal.zero()
+  pool.volumeUSD = BigDecimal.zero()
+  pool.txCount = BigInt.zero()
+  pool.totalValueLockedUSD = BigDecimal.zero()
+  pool.createdAtBlock = event.block.number
+  pool.createdAtTimestamp = event.block.timestamp
+  pool.save()
 }
 
 /******************************
- * V2 Factory Handlers (Uni/Sushi/QuickSwap)
+ * V2 Factory Handlers
  ******************************/
 export function handleV2PairCreated(event: V2PairCreatedEvent): void {
-  let entity = new PairCreated(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  // Event history
+  let entity = new PairCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
   entity.token0 = event.params.token0
   entity.token1 = event.params.token1
   entity.pair = event.params.pair
-  entity.param3 = event.params.param3 // last uint256
+  entity.param3 = event.params.param3
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
   entity.save()
+
+  // ***** ACTUAL POOL CREATION *****
+  let pool = new Pool(event.params.pair)
+  pool.token0 = event.params.token0
+  pool.token1 = event.params.token1
+  pool.dexType = "v2"
+  pool.reserve0 = BigDecimal.zero()
+  pool.reserve1 = BigDecimal.zero()
+  pool.reserveUSD = BigDecimal.zero()
+  pool.volumeUSD = BigDecimal.zero()
+  pool.txCount = BigInt.zero()
+  pool.totalValueLockedUSD = BigDecimal.zero()
+  pool.createdAtBlock = event.block.number
+  pool.createdAtTimestamp = event.block.timestamp
+  pool.save()
 }
