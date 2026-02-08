f002ca64a33e2e65da07d9b2a0b7b341e639db25
sequelinquiryDev <sequelinquiry@gmail.com>
2026-02-08 18:33:17 +0200

    Aktualisieren von PairsAndPools.ts

--- mine/src/PairsAndPools.ts
+++ mine/src/PairsAndPools.ts
@@ -37,16 +37,13 @@ function initializePool(poolId: string): Pool {
   return entity as Pool
 }
 
-// Placeholder: replace with your price oracle logic
 function getTokenPriceUSD(tokenAddress: string): BigDecimal {
-  // Example: fixed dummy price for testing
-  return BigDecimal.fromString("1") // 1 USD
+  return BigDecimal.fromString("1") // placeholder
 }
 
 function updateUSDValues(pool: Pool): void {
   let token0Price = getTokenPriceUSD(pool.token0.toHex())
   let token1Price = getTokenPriceUSD(pool.token1.toHex())
-
   pool.reserveUSD = pool.reserve0.times(token0Price).plus(pool.reserve1.times(token1Price))
   pool.totalValueLockedUSD = pool.reserveUSD
 }
@@ -55,46 +52,37 @@ function updateUSDValues(pool: Pool): void {
  * V2 Handlers
  ******************************/
 export function handleSync(event: V2SyncEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
+  let pool = initializePool(event.address.toHex())
   pool.reserve0 = bigIntToBigDecimal(event.params.reserve0)
   pool.reserve1 = bigIntToBigDecimal(event.params.reserve1)
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
   updateUSDValues(pool)
   pool.save()
 }
 
 export function handleSwapV2(event: V2SwapEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
+  let pool = initializePool(event.address.toHex())
   pool.txCount = pool.txCount.plus(BigInt.fromI32(1))
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
-  // Sync will update reserves
   updateUSDValues(pool)
   pool.save()
 }
 
 export function handleMintV2(event: V2MintEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
+  let pool = initializePool(event.address.toHex())
+  updateUSDValues(pool)
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
-  updateUSDValues(pool)
   pool.save()
 }
 
 export function handleBurnV2(event: V2BurnEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
+  let pool = initializePool(event.address.toHex())
+  updateUSDValues(pool)
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
-  updateUSDValues(pool)
   pool.save()
 }
 
@@ -102,44 +90,32 @@ export function handleBurnV2(event: V2BurnEvent): void {
  * V3 Handlers
  ******************************/
 export function handleSwapV3(event: V3SwapEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
+  let pool = initializePool(event.address.toHex())
   pool.txCount = pool.txCount.plus(BigInt.fromI32(1))
-
-  let amount0 = bigIntToBigDecimal(event.params.amount0)
-  let amount1 = bigIntToBigDecimal(event.params.amount1)
-  pool.reserve0 = pool.reserve0.minus(amount0)
-  pool.reserve1 = pool.reserve1.minus(amount1)
-
+  pool.reserve0 = pool.reserve0.minus(bigIntToBigDecimal(event.params.amount0))
+  pool.reserve1 = pool.reserve1.minus(bigIntToBigDecimal(event.params.amount1))
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
   updateUSDValues(pool)
   pool.save()
 }
 
 export function handleMintV3(event: V3MintEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
-
+  let pool = initializePool(event.address.toHex())
   pool.reserve0 = pool.reserve0.plus(bigIntToBigDecimal(event.params.amount0))
   pool.reserve1 = pool.reserve1.plus(bigIntToBigDecimal(event.params.amount1))
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
   updateUSDValues(pool)
   pool.save()
 }
 
 export function handleBurnV3(event: V3BurnEvent): void {
-  let poolId = event.address.toHex()
-  let pool = initializePool(poolId)
-
+  let pool = initializePool(event.address.toHex())
   pool.reserve0 = pool.reserve0.minus(bigIntToBigDecimal(event.params.amount0))
   pool.reserve1 = pool.reserve1.minus(bigIntToBigDecimal(event.params.amount1))
   pool.blockNumber = event.block.number
   pool.blockTimestamp = event.block.timestamp
-
   updateUSDValues(pool)
   pool.save()
 }
